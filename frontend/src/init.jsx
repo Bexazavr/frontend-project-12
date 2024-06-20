c7db5b65fd455d5060d8f719f4a03fbbaa2d33ce
Vitaly <bexins@gmail.com>
2024-06-21 00:59:49 +0300

    add i18next, react-i18next

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,15 +1,27 @@
+import i18next from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import resources from "./locales/index.js";
 import { Provider } from "react-redux";
 import store from "./slices/index.js";
 import App from "./components/App";
 import { socket, WebSocketContext } from "./context/webSocketContext.js";
 
-const init = () => {
+const init = async () => {
+  const i18n = i18next.createInstance();
+
+  await i18n.use(initReactI18next).init({
+    resources,
+    fallbackLng: "ru",
+  });
+
   return (
-    <Provider store={store}>
-      <WebSocketContext.Provider value={socket}>
-        <App />
-      </WebSocketContext.Provider>
-    </Provider>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <WebSocketContext.Provider value={socket}>
+          <App />
+        </WebSocketContext.Provider>
+      </Provider>
+    </I18nextProvider>
   );
 };
 export default init;
